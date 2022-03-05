a7f710eedfff54d9219280fa326255d8ea33809b
GennadiyProg <grumancev9@gmail.com>
2022-03-05 19:05:45 +0300

    mega fix

--- src/main/java/ru/corporateproduct/raceth/Main.java
+++ src/main/java/ru/corporateproduct/raceth/Main.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
 
 import java.io.IOException;
 
