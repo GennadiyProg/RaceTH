41049dc8b8c50a2590b379305a9f86b16682dc4b
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2022-03-09 21:26:45 +0300

    Добавлен метод для парсинга ID меток

--- src/main/java/ru/corporateproduct/raceth/usbreader/UsbReader.java
+++ src/main/java/ru/corporateproduct/raceth/usbreader/UsbReader.java
@@ -1,70 +1,50 @@
 package ru.corporateproduct.raceth.usbreader;
 
-import org.usb4java.Device;
-import org.usb4java.DeviceList;
-import org.usb4java.LibUsb;
-import org.usb4java.javax.Services;
-
-import javax.usb.*;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UsbReader {
 
-    private final UsbHub usbHub = new Services().getRootUsbHub();
-    private final short vendorId;
-    private final short productId;
+    public String IdParser(String id) {
+        char[] idArray = id.toCharArray();
 
-    public UsbReader(short vId, short pId) throws UsbException {
-        this.vendorId = vId;
-        this.productId = pId;
-    }
+        for (int i = 0; i < idArray.length; i++) {
+            char letter = Character.toLowerCase(idArray[i]);
 
-    private UsbDevice FindDevice() {
-        return this.FindDevice(usbHub, this.vendorId, this.productId);
-    }
+            if (Character.isDigit(letter)) continue;
+            if (97 <= (int) letter && (int) letter <= 122) {
+                idArray[i] = letter;
+                continue;
+            }
 
-    private UsbDevice FindDevice(UsbHub hub, short vendorId, short productId) {
-        List<UsbDevice> devices = hub.getAttachedUsbDevices();
-        for (UsbDevice device : devices) {
-            UsbDeviceDescriptor dev = device.getUsbDeviceDescriptor();
-            if (dev.idVendor() == vendorId && dev.idProduct() == productId)
-                return device;
-            if (device.isUsbHub())
-                FindDevice((UsbHub) device, vendorId, productId);
+                 if (letter == 'й') idArray[i] = 'q';
+            else if (letter == 'ц') idArray[i] = 'w';
+            else if (letter == 'у') idArray[i] = 'e';
+            else if (letter == 'к') idArray[i] = 'r';
+            else if (letter == 'е') idArray[i] = 't';
+            else if (letter == 'н') idArray[i] = 'y';
+            else if (letter == 'г') idArray[i] = 'u';
+            else if (letter == 'ш') idArray[i] = 'i';
+            else if (letter == 'щ') idArray[i] = 'o';
+            else if (letter == 'з') idArray[i] = 'p';
+            else if (letter == 'ф') idArray[i] = 'a';
+            else if (letter == 'ы') idArray[i] = 's';
+            else if (letter == 'в') idArray[i] = 'd';
+            else if (letter == 'а') idArray[i] = 'f';
+            else if (letter == 'п') idArray[i] = 'g';
+            else if (letter == 'р') idArray[i] = 'h';
+            else if (letter == 'о') idArray[i] = 'j';
+            else if (letter == 'л') idArray[i] = 'k';
+            else if (letter == 'д') idArray[i] = 'l';
+            else if (letter == 'я') idArray[i] = 'z';
+            else if (letter == 'ч') idArray[i] = 'x';
+            else if (letter == 'с') idArray[i] = 'c';
+            else if (letter == 'м') idArray[i] = 'v';
+            else if (letter == 'и') idArray[i] = 'b';
+            else if (letter == 'т') idArray[i] = 'n';
+            else if (letter == 'ь') idArray[i] = 'm';
         }
-        return null;
-    }
 
-    public void ReceiveMessage() {
-
-    }
-
-    public void main() throws UsbException {
-        UsbDevice device = FindDevice();
-        System.out.println(device.getUsbDeviceDescriptor());
-
-        UsbInterface infc = device.getActiveUsbConfiguration().getUsbInterface((byte) 0);
-
-        infc.claim();
-//        infc.claim(new UsbInterfacePolicy() {
-//                       @Override
-//                       public boolean forceClaim(UsbInterface usbInterface) {
-//                           return true;
-//                       }
-//                   }
-//        );
-
-        UsbEndpoint receptionEndPoint = (UsbEndpoint) infc.getUsbEndpoints().get(0);
-
-        UsbPipe receptionPipe = receptionEndPoint.getUsbPipe();
-        receptionPipe.open();
-
-        byte[] b = new byte[64];
-        int length = 27;
-        while (true) {
-            var Длина = receptionPipe.syncSubmit(b);
-            for (byte i : b)
-                System.out.println(i);
-        }
+        return String.valueOf(idArray);
     }
 }
