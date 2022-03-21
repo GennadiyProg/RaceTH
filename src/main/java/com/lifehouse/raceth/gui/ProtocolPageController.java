bf0a4b2862befe80c1af673f84b7e0ad35405c5c
Be1t0k <pasha.moshckov@yandex.ru>
2022-03-21 18:00:14 +0300

    add protocol_page

---
+++ src/main/java/com/lifehouse/raceth/gui/ProtocolPageController.java
@@ -0,0 +1,11 @@
+package com.lifehouse.raceth.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import lombok.Data;
+
+@Data
+public class ProtocolPageController {
+    @FXML
+    private AnchorPane main_pane;
+}
