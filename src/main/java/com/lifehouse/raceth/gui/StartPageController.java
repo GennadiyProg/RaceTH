fa506465d319a9ebfec88bcd40b2ec0ab6534ebc
GennadiyProg <grumancev9@gmail.com>
2022-03-21 14:43:43 +0300

    added switching page

--- src/main/java/com/lifehouse/raceth/gui/StartPageController.java
+++ src/main/java/com/lifehouse/raceth/gui/StartPageController.java
@@ -4,12 +4,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartPageController implements Initializable {
 
+    @FXML
+    public AnchorPane competition_page;
+    @FXML
+    public AnchorPane run_page;
     //Вехнее меню выбора
     @FXML
     private ComboBox<String> ActiveComp;
@@ -23,18 +28,12 @@ public class StartPageController implements Initializable {
     }
 
     @FXML
-    private RunPageController runPageController;
-
-    @FXML
-    private CompetitionPageController competitionPageController;
-
-    @FXML
     private void handleButtonAction(ActionEvent event) {
-        competitionPageController.getMain_pane().toFront();
+        competition_page.toFront();
     }
 
     @FXML
     private void handleButtonAction2(ActionEvent event) {
-        runPageController.getMain_pane().toFront();
+        run_page.toFront();
     }
 }
