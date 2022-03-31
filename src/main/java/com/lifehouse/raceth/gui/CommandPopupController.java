b9a69a84b7130120967d42e6416120e4bb2011b6
Naramotik <AntwoodLive@yandex.ru>
2022-03-31 18:08:54 +0300

    Создан рабочий попап

--- src/main/java/com/lifehouse/raceth/gui/CommandPopupController.java
+++ src/main/java/com/lifehouse/raceth/gui/CommandPopupController.java
@@ -1,7 +1,11 @@
 package com.lifehouse.raceth.gui;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import lombok.Data;
@@ -11,6 +15,33 @@ public class CommandPopupController {
     @FXML
     private AnchorPane main_pane;
 
+    @FXML
+    private Button cancelButt;
 
+    @FXML
+    private Button saveButt;
+
+    @FXML
+    private TextField commandName;
+
+    @FXML
+    void Saving(ActionEvent event) {
+        try{
+            System.out.println(commandName.getText()); //Сохранения названия
+            ((Node)(event.getSource())).getScene().getWindow().hide(); //Закрытие окна
+        } catch (Exception e) {
+            System.out.println("cant loading");
+        }
+    }
+
+    @FXML
+    void Cancel(ActionEvent event) {
+        try{
+            ((Node)(event.getSource())).getScene().getWindow().hide(); //Закрытие окна
+        } catch (Exception e) {
+            System.out.println("cant loading");
+        }
+    }
 }
+
 
