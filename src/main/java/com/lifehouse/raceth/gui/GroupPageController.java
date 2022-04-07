4973c3d7f567b196335f588d36462ec823dbe133
Be1t0k <pasha.moshckov@yandex.ru>
2022-04-07 15:28:24 +0300

    create groups and clubs

---
+++ src/main/java/com/lifehouse/raceth/gui/GroupPageController.java
@@ -0,0 +1,14 @@
+package com.lifehouse.raceth.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import lombok.Data;
+
+@Data
+public class GroupPageController {
+    @FXML
+    private AnchorPane main_pane;
+
+
+}
+
