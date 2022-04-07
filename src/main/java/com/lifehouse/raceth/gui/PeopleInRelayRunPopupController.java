52eda63808ffa261fe884a34dee056ce4fe4c12c
Naramotik <AntwoodLive@yandex.ru>
2022-04-07 16:00:55 +0300

    попап эстафета

--- src/main/java/com/lifehouse/raceth/gui/PeopleInRelayRunPopupController.java
+++ src/main/java/com/lifehouse/raceth/gui/PeopleInRelayRunPopupController.java
@@ -1,25 +1,17 @@
 package com.lifehouse.raceth.gui;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import lombok.Data;
 
 @Data
-public class CommandPopupController {
+public class PeopleInRelayRunPopupController {
     @FXML
     private AnchorPane main_pane;
-
-    @FXML
-    private Button cancelButt;
-
-    @FXML
-    private Button saveButt;
 
     @FXML
     private TextField commandName;
@@ -27,7 +19,7 @@ public class CommandPopupController {
     @FXML
     void Saving(ActionEvent event) {
         try{
-            System.out.println(commandName.getText()); //Сохранения названия
+            System.out.println(commandName.getText());                 //Добавление участника
             ((Node)(event.getSource())).getScene().getWindow().hide(); //Закрытие окна
         } catch (Exception e) {
             System.out.println("cant loading");
@@ -36,7 +28,7 @@ public class CommandPopupController {
 
     @FXML
     void Cancel(ActionEvent event) {
-        try{
+        try{                                                           //Отмена
             ((Node)(event.getSource())).getScene().getWindow().hide(); //Закрытие окна
         } catch (Exception e) {
             System.out.println("cant loading");
