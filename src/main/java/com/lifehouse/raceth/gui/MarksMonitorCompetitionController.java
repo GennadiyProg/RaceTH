ee9ec2767e5c526e2fdbd94c5d583f26b9900ada
Be1t0k <pasha.moshckov@yandex.ru>
2022-04-16 12:15:34 +0300

    create dinamic tabs

--- src/main/java/com/lifehouse/raceth/gui/MarksMonitorCompetitionController.java
+++ src/main/java/com/lifehouse/raceth/gui/MarksMonitorCompetitionController.java
@@ -10,12 +10,23 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import lombok.Data;
+import javafx.scene.control.*;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
-public class MarksMonitorCompetitionController {
+public class MarksMonitorCompetitionController implements Initializable {
 
     @FXML
     private Button addGroup;
+
+    @FXML
+    private  TabPane tabPane;
 
     @FXML
     private AnchorPane main_pane;
@@ -35,5 +46,56 @@ public class MarksMonitorCompetitionController {
         }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+            Tab tab = new Tab("Tab_");
 
-}
+            // create a label
+            Label label = new Label("This is Tab: ");
+
+
+            // add label to the tab
+            tab.setContent(label);
+
+            // add tab
+            tabPane.getTabs().add(tab);
+
+            // create a tab which
+            // when pressed creates a new tab
+            Tab newtab = new Tab();
+
+
+            EventHandler<Event> event = new EventHandler<Event>() {
+
+                public void handle(Event e)
+                {
+                    if (newtab.isSelected())
+                    {
+
+                        // create Tab
+                        Tab tab = new Tab("Tab_");
+
+                        // create a label
+                        Label label = new Label("This is Tab: ");
+
+
+
+                        // add label to the tab
+                        tab.setContent(label);
+
+                        // add tab
+                        tabPane.getTabs().add(tabPane.getTabs().size() - 1, tab);
+
+                        // select the last tab
+                        tabPane.getSelectionModel().select(tabPane.getTabs().size() - 2);
+                    }
+                }
+            };
+
+            // set event handler to the tab
+            newtab.setOnSelectionChanged(event);
+
+            // add newtab
+            tabPane.getTabs().add(newtab);
+        };
+};
