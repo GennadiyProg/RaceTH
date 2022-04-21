23e62c1685d14bb74b859022d0aec826f7f8937b
Denis <d.igoschin.igoshin@gmail.com>
2022-04-21 14:11:03 +0300

    Настроены стили для ChoiseBox

--- src/main/java/com/lifehouse/raceth/gui/ClubPopupController.java
+++ src/main/java/com/lifehouse/raceth/gui/ClubPopupController.java
@@ -1,7 +1,10 @@
 package com.lifehouse.raceth.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -9,8 +12,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import lombok.Data;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Data
-public class ClubPopupController {
+public class ClubPopupController implements Initializable {
     @FXML
     private AnchorPane main_pane;
     @FXML
@@ -18,10 +24,13 @@ public class ClubPopupController {
     @FXML
     private TextField coach;
     @FXML
-    private ChoiceBox<?> region;
+    private ChoiceBox<String> region;
     @FXML
     private Button regionButt;
 
+    public void initialize(URL var1, ResourceBundle var2) {
+        region.getItems().addAll("item1", "item2", "item3");
+    }
 
     @FXML
     void Saving(ActionEvent event) {
