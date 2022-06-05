cc3fc12b3b98d5308a634ed1040b656f962bce25
gggchaseggg <gggchaseggg@gmail.com>
2022-06-05 14:53:12 +0300

    fix таблицы участников

--- src/main/java/com/lifehouse/raceth/model/view/ParticipantCompetitionView.java
+++ src/main/java/com/lifehouse/raceth/model/view/ParticipantCompetitionView.java
@@ -26,28 +26,6 @@ public class ParticipantCompetitionView {
     private String discharge;
     private String group;
 
-    public ParticipantCompetitionView(
-            long id,
-            String name,
-            String lastname,
-            String patronymic,
-            Gender gender,
-            String chip,
-            int startNumber,
-            LocalDate birthdate,
-            String region,
-            String group) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.patronymic = patronymic;
-        this.gender = gender;
-        this.chip = chip;
-        this.startNumber = startNumber;
-        this.birthdate = birthdate;
-        this.region = region;
-        this.group = group;
-    }
 
     public static ParticipantCompetitionView convertToView(Participant participant){
         return new ParticipantCompetitionView(
@@ -60,6 +38,8 @@ public class ParticipantCompetitionView {
                 participant.getStartNumber(),
                 participant.getSportsman().getBirthdate(),
                 participant.getSportsman().getRegion(),
+                participant.getClub(),
+                participant.getSportsman().getDischarge(),
                 participant.getStart().getGroup().getName()
         );
     }
