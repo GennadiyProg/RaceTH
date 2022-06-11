6a2799418103e1967544607f708e6bcf7d71fc84
GennadiyProg <grumancev9@gmail.com>
2022-06-11 18:46:03 +0300

    Small fix dependencies

--- setting.bat
+++ setting.bat
@@ -1,7 +1,7 @@
 .\wget "https://download.oracle.com/java/17/archive/jdk-17.0.3.1_windows-x64_bin.msi"
 .\wget "https://download2.gluonhq.com/openjfx/17.0.2/openjfx-17.0.2_windows-x64_bin-sdk.zip"
 
-7za x "openjfx-17.0.2_windows-x64_bin-sdk.zip" -o"javafx-sdk-17.0.2"
+7za x "openjfx-17.0.2_windows-x64_bin-sdk.zip"
 
 chcp 65001
 echo Не выключайте терминал до полной установки Java
@@ -10,4 +10,4 @@ echo Не выключайте терминал до полной установ
 del "jdk-17.0.3.1_windows-x64_bin.msi"
 del "openjfx-17.0.2_windows-x64_bin-sdk.zip"
 
-java --module-path .\javafx-sdk-17.0.2\lib --add-modules javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar .\RaceTH-1.0-SNAPSHOT-jar-with-dependencies.jar
+java --module-path .\javafx-sdk-17.0.2\lib --add-modules javafx.controls,javafx.base,javafx.fxml -jar .\RaceTH-1.0-SNAPSHOT-jar-with-dependencies.jar
